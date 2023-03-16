9dbb3c21f88df6aefa0e50f9ca5bcc0877eceaa7
fbrunoobrito <brunoarphenia@outlook.com>
2023-03-15 21:03:10 -0300

    Implementing edit page and updating SQL DB

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -8,11 +8,11 @@ router.get("/admin/categories/new", (req, res) => {
 });
 
 router.post("/categories/save", (req, res) => {
-  var title = req.body.title.toLowerCase();
+  var title = req.body.title;
   if (title != undefined) {
     Category.create({
       title,
-      slug: slugify(title),
+      slug: slugify(title).toLowerCase(),
     }).then(() => res.redirect("/admin/categories/"));
   } else {
     res.redirect("/admin/categories/new");
@@ -45,6 +45,42 @@ router.post("/categories/delete", (req, res) => {
     //Se o ID for Null
     res.redirect("/admin/categories");
   }
+});
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+  var id = req.params.id;
+
+  if (isNaN(id)) {
+    res.redirect("/admin/categories");
+  }
+
+  Category.findByPk(id)
+    .then((category) => {
+      if (category != undefined) {
+        res.render("admin/categories/edit", { category: category });
+      } else {
+        res.redirect("/admin/categories");
+      }
+    })
+    .catch((erro) => {
+      res.redirect("/admin/categories");
+    });
+});
+
+router.post("/categories/update", (req, res) => {
+  var id = req.body.id;
+  var title = req.body.title;
+
+  Category.update(
+    { title: title, slug: slugify(title).toLowerCase() },
+    {
+      where: {
+        id: id,
+      },
+    }
+  ).then(() => {
+    res.redirect("/admin/categories");
+  });
 });
 
 module.exports = router;
