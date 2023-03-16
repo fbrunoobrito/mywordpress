c0a803cd590bd6f32a626d01fc2f1cca7d1b0c9a
fbrunoobrito <brunoarphenia@outlook.com>
2023-03-15 22:04:04 -0300

    Create Article Page and Create Entry on SQL DB

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
-router.get("/articles", function (req, res) {
+router.get("/admin/articles", function (req, res) {
   res.send("Aqui Artigos");
 });
 
 router.get("/admin/articles/new", function (req, res) {
-  res.send("Aqui Artigos");
+  Category.findAll().then((categories) => {
+    res.render("admin/articles/new", { categories: categories });
+  });
+});
+
+router.post("/articles/save", (req, res) => {
+  var title = req.body.title;
+  var body = req.body.body;
+  var category = req.body.category;
+
+  Article.create({
+    title,
+    slug: slugify(title).toLowerCase(),
+    body,
+    categoryId: category,
+  }).then(() => {
+    res.redirect("/admin/articles");
+  });
 });
 
 module.exports = router;
